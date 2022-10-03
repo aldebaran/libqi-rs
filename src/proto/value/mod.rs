38a718c2b6f119df70b18d586a5746f5d1600ff9
Vincent Palancher <vincent.palancher@aldebaran.com>
2022-10-03 20:13:56 +0200

    [proto::value] Implement value serialization

--- src/proto/value/mod.rs
+++ src/proto/value/mod.rs
@@ -2,7 +2,6 @@ mod de;
 mod ser;
 
 use super::Object;
-use std::collections::HashMap;
 use thiserror::Error;
 
 //pub enum Type {
@@ -29,9 +28,16 @@ use thiserror::Error;
 //    Unknown,
 //}
 
+#[derive(Debug)]
 struct TupleMember {
     name: Option<String>,
     value: Value,
+}
+
+impl PartialEq for TupleMember {
+    fn eq(&self, other: &Self) -> bool {
+        self.name == other.name && self.value == other.value
+    }
 }
 
 pub enum Value {
@@ -43,18 +49,86 @@ pub enum Value {
     UInt16(u16),
     Int32(i32),
     UInt32(u32),
+    Int64(i64),
+    UInt64(u64),
     Float(f32),
     Double(f64),
     String(String),
     List(Vec<Value>),
-    Map(HashMap<Value, Value>),
+    Map(Vec<(Value, Value)>),
     Object(Box<dyn Object>),
     Tuple {
         name: Option<String>,
         members: Vec<TupleMember>,
     },
     Raw(Vec<u8>),
-    Optional(Box<Value>),
+    Optional(Option<Box<Value>>),
+}
+
+impl PartialEq for Value {
+    fn eq(&self, other: &Self) -> bool {
+        match (self, other) {
+            (Self::Bool(l0), Self::Bool(r0)) => l0 == r0,
+            (Self::Int8(l0), Self::Int8(r0)) => l0 == r0,
+            (Self::UInt8(l0), Self::UInt8(r0)) => l0 == r0,
+            (Self::Int16(l0), Self::Int16(r0)) => l0 == r0,
+            (Self::UInt16(l0), Self::UInt16(r0)) => l0 == r0,
+            (Self::Int32(l0), Self::Int32(r0)) => l0 == r0,
+            (Self::UInt32(l0), Self::UInt32(r0)) => l0 == r0,
+            (Self::Int64(l0), Self::Int64(r0)) => l0 == r0,
+            (Self::UInt64(l0), Self::UInt64(r0)) => l0 == r0,
+            (Self::Float(l0), Self::Float(r0)) => l0 == r0,
+            (Self::Double(l0), Self::Double(r0)) => l0 == r0,
+            (Self::String(l0), Self::String(r0)) => l0 == r0,
+            (Self::List(l0), Self::List(r0)) => l0 == r0,
+            (Self::Map(l0), Self::Map(r0)) => l0 == r0,
+            // Objects are not comparable
+            (Self::Object(l0), Self::Object(r0)) => false,
+            (
+                Self::Tuple {
+                    name: l_name,
+                    members: l_members,
+                },
+                Self::Tuple {
+                    name: r_name,
+                    members: r_members,
+                },
+            ) => l_name == r_name && l_members == r_members,
+            (Self::Raw(l0), Self::Raw(r0)) => l0 == r0,
+            (Self::Optional(l0), Self::Optional(r0)) => l0 == r0,
+            _ => core::mem::discriminant(self) == core::mem::discriminant(other),
+        }
+    }
+}
+
+impl std::fmt::Debug for Value {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Void => write!(f, "Void"),
+            Self::Bool(arg0) => f.debug_tuple("Bool").field(arg0).finish(),
+            Self::Int8(arg0) => f.debug_tuple("Int8").field(arg0).finish(),
+            Self::UInt8(arg0) => f.debug_tuple("UInt8").field(arg0).finish(),
+            Self::Int16(arg0) => f.debug_tuple("Int16").field(arg0).finish(),
+            Self::UInt16(arg0) => f.debug_tuple("UInt16").field(arg0).finish(),
+            Self::Int32(arg0) => f.debug_tuple("Int32").field(arg0).finish(),
+            Self::UInt32(arg0) => f.debug_tuple("UInt32").field(arg0).finish(),
+            Self::Int64(arg0) => f.debug_tuple("Int64").field(arg0).finish(),
+            Self::UInt64(arg0) => f.debug_tuple("UInt64").field(arg0).finish(),
+            Self::Float(arg0) => f.debug_tuple("Float").field(arg0).finish(),
+            Self::Double(arg0) => f.debug_tuple("Double").field(arg0).finish(),
+            Self::String(arg0) => f.debug_tuple("String").field(arg0).finish(),
+            Self::List(arg0) => f.debug_tuple("List").field(arg0).finish(),
+            Self::Map(arg0) => f.debug_tuple("Map").field(arg0).finish(),
+            Self::Object(arg0) => f.write_str("Object"),
+            Self::Tuple { name, members } => f
+                .debug_struct("Tuple")
+                .field("name", name)
+                .field("members", members)
+                .finish(),
+            Self::Raw(arg0) => f.debug_tuple("Raw").field(arg0).finish(),
+            Self::Optional(arg0) => f.debug_tuple("Optional").field(arg0).finish(),
+        }
+    }
 }
 
 fn to_value<T>(value: &T) -> Result<Value>
@@ -77,6 +151,12 @@ where
 pub enum Error {
     #[error("error: {0}")]
     Custom(String),
+
+    #[error("union types are not supported in the qi type system")]
+    UnionAreNotSupported,
+
+    #[error("a map key is missing")]
+    MissingMapKey,
 }
 
 impl serde::ser::Error for Error {
@@ -92,18 +172,3 @@ impl serde::de::Error for Error {
 }
 
 type Result<T> = std::result::Result<T, Error>;
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn to_value() {
-        todo!()
-    }
-
-    #[test]
-    fn from_value() {
-        todo!()
-    }
-}
--- src/proto/value/ser.rs
+++ src/proto/value/ser.rs
@@ -1,4 +1,4 @@
-use super::{Error, Value};
+use super::{to_value, Error, TupleMember, Value};
 
 pub struct Serializer;
 
@@ -8,94 +8,101 @@ impl serde::Serializer for Serializer {
 
     type SerializeSeq = ListSerializer;
     type SerializeTuple = TupleSerializer;
-    type SerializeTupleStruct = TupleStructSerializer;
-    type SerializeTupleVariant = TupleVariantSerializer;
+    type SerializeTupleStruct = TupleSerializer;
+    type SerializeTupleVariant = serde::ser::Impossible<Self::Ok, Self::Error>;
     type SerializeMap = MapSerializer;
-    type SerializeStruct = StructSerializer;
-    type SerializeStructVariant = StructVariantSerializer;
+    type SerializeStruct = TupleSerializer;
+    type SerializeStructVariant = serde::ser::Impossible<Self::Ok, Self::Error>;
 
     fn serialize_bool(self, v: bool) -> Result<Self::Ok, Self::Error> {
-        todo!()
+        Ok(Value::Bool(v))
     }
 
     fn serialize_i8(self, v: i8) -> Result<Self::Ok, Self::Error> {
-        todo!()
+        Ok(Value::Int8(v))
     }
 
     fn serialize_i16(self, v: i16) -> Result<Self::Ok, Self::Error> {
-        todo!()
+        Ok(Value::Int16(v))
     }
 
     fn serialize_i32(self, v: i32) -> Result<Self::Ok, Self::Error> {
-        todo!()
+        Ok(Value::Int32(v))
     }
 
     fn serialize_i64(self, v: i64) -> Result<Self::Ok, Self::Error> {
-        todo!()
+        Ok(Value::Int64(v))
     }
 
     fn serialize_u8(self, v: u8) -> Result<Self::Ok, Self::Error> {
-        todo!()
+        Ok(Value::UInt8(v))
     }
 
     fn serialize_u16(self, v: u16) -> Result<Self::Ok, Self::Error> {
-        todo!()
+        Ok(Value::UInt16(v))
     }
 
     fn serialize_u32(self, v: u32) -> Result<Self::Ok, Self::Error> {
-        todo!()
+        Ok(Value::UInt32(v))
     }
 
     fn serialize_u64(self, v: u64) -> Result<Self::Ok, Self::Error> {
-        todo!()
+        Ok(Value::UInt64(v))
     }
 
     fn serialize_f32(self, v: f32) -> Result<Self::Ok, Self::Error> {
-        todo!()
+        Ok(Value::Float(v))
     }
 
     fn serialize_f64(self, v: f64) -> Result<Self::Ok, Self::Error> {
-        todo!()
+        Ok(Value::Double(v))
     }
 
     fn serialize_char(self, v: char) -> Result<Self::Ok, Self::Error> {
-        todo!()
+        let mut buf = [0; 4];
+        let s = v.encode_utf8(&mut buf);
+        self.serialize_str(s)
     }
 
     fn serialize_str(self, v: &str) -> Result<Self::Ok, Self::Error> {
-        todo!()
+        Ok(Value::String(v.to_string()))
     }
 
     fn serialize_bytes(self, v: &[u8]) -> Result<Self::Ok, Self::Error> {
-        todo!()
+        // OPTIMIZE: Do not copy bytes, but reference them
+        Ok(Value::Raw(v.into()))
     }
 
     fn serialize_none(self) -> Result<Self::Ok, Self::Error> {
-        todo!()
+        Ok(Value::Optional(None))
     }
 
     fn serialize_some<T: ?Sized>(self, value: &T) -> Result<Self::Ok, Self::Error>
     where
         T: serde::Serialize,
     {
-        todo!()
+        let value = to_value(value)?;
+        Ok(Value::Optional(Some(Box::new(value))))
     }
 
     fn serialize_unit(self) -> Result<Self::Ok, Self::Error> {
-        todo!()
+        Ok(Value::Void)
     }
 
     fn serialize_unit_struct(self, name: &'static str) -> Result<Self::Ok, Self::Error> {
-        todo!()
+        Ok(Value::Tuple {
+            name: Some(name.to_string()),
+            members: vec![],
+        })
     }
 
     fn serialize_unit_variant(
         self,
-        name: &'static str,
-        variant_index: u32,
-        variant: &'static str,
+        _name: &'static str,
+        _variant_index: u32,
+        _variant: &'static str,
     ) -> Result<Self::Ok, Self::Error> {
-        todo!()
+        Err(Error::UnionAreNotSupported)
     }
 
     fn serialize_newtype_struct<T: ?Sized>(
@@ -106,72 +113,86 @@ impl serde::Serializer for Serializer {
     where
         T: serde::Serialize,
     {
-        todo!()
+        let value = to_value(value)?;
+        Ok(Value::Tuple {
+            name: Some(name.to_string()),
+            members: vec![TupleMember { name: None, value }],
+        })
     }
 
     fn serialize_newtype_variant<T: ?Sized>(
         self,
-        name: &'static str,
-        variant_index: u32,
-        variant: &'static str,
-        value: &T,
+        _name: &'static str,
+        _variant_index: u32,
+        _variant: &'static str,
+        _value: &T,
     ) -> Result<Self::Ok, Self::Error>
     where
         T: serde::Serialize,
     {
-        todo!()
+        Err(Error::UnionAreNotSupported)
     }
 
-    fn serialize_seq(self, len: Option<usize>) -> Result<Self::SerializeSeq, Self::Error> {
-        todo!()
+    fn serialize_seq(self, _len: Option<usize>) -> Result<Self::SerializeSeq, Self::Error> {
+        Ok(ListSerializer::new())
     }
 
-    fn serialize_tuple(self, len: usize) -> Result<Self::SerializeTuple, Self::Error> {
-        todo!()
+    fn serialize_tuple(self, _len: usize) -> Result<Self::SerializeTuple, Self::Error> {
+        Ok(TupleSerializer::new(None))
     }
 
     fn serialize_tuple_struct(
         self,
         name: &'static str,
-        len: usize,
+        _len: usize,
     ) -> Result<Self::SerializeTupleStruct, Self::Error> {
-        todo!()
+        Ok(TupleSerializer::new(Some(name.to_string())))
     }
 
     fn serialize_tuple_variant(
         self,
-        name: &'static str,
-        variant_index: u32,
-        variant: &'static str,
-        len: usize,
+        _name: &'static str,
+        _variant_index: u32,
+        _variant: &'static str,
+        _len: usize,
     ) -> Result<Self::SerializeTupleVariant, Self::Error> {
-        todo!()
+        Err(Error::UnionAreNotSupported)
     }
 
-    fn serialize_map(self, len: Option<usize>) -> Result<Self::SerializeMap, Self::Error> {
-        todo!()
+    fn serialize_map(self, _len: Option<usize>) -> Result<Self::SerializeMap, Self::Error> {
+        Ok(MapSerializer::new())
     }
 
     fn serialize_struct(
         self,
         name: &'static str,
-        len: usize,
+        _len: usize,
     ) -> Result<Self::SerializeStruct, Self::Error> {
-        todo!()
+        Ok(TupleSerializer::new(Some(name.to_string())))
     }
 
     fn serialize_struct_variant(
         self,
-        name: &'static str,
-        variant_index: u32,
-        variant: &'static str,
-        len: usize,
+        _name: &'static str,
+        _variant_index: u32,
+        _variant: &'static str,
+        _len: usize,
     ) -> Result<Self::SerializeStructVariant, Self::Error> {
-        todo!()
+        Err(Error::UnionAreNotSupported)
     }
 }
 
-pub struct ListSerializer;
+pub struct ListSerializer {
+    elements: Vec<Value>,
+}
+
+impl ListSerializer {
+    fn new() -> Self {
+        Self {
+            elements: Vec::new(),
+        }
+    }
+}
 
 impl serde::ser::SerializeSeq for ListSerializer {
     type Ok = Value;
@@ -181,69 +202,29 @@ impl serde::ser::SerializeSeq for ListSerializer {
     where
         T: serde::Serialize,
     {
-        todo!()
+        let value = to_value(value)?;
+        self.elements.push(value);
+        Ok(())
     }
 
     fn end(self) -> Result<Self::Ok, Self::Error> {
-        todo!()
+        Ok(Value::List(self.elements))
     }
 }
 
-pub struct TupleSerializer;
-
-impl serde::ser::SerializeTuple for TupleSerializer {
-    type Ok = Value;
-    type Error = Error;
-
-    fn serialize_element<T: ?Sized>(&mut self, value: &T) -> Result<(), Self::Error>
-    where
-        T: serde::Serialize,
-    {
-        todo!()
-    }
-
-    fn end(self) -> Result<Self::Ok, Self::Error> {
-        todo!()
-    }
+pub struct MapSerializer {
+    elements: Vec<(Value, Value)>,
+    key: Option<Value>,
 }
 
-pub struct TupleStructSerializer;
-
-impl serde::ser::SerializeTupleStruct for TupleStructSerializer {
-    type Ok = Value;
-    type Error = Error;
-
-    fn serialize_field<T: ?Sized>(&mut self, value: &T) -> Result<(), Self::Error>
-    where
-        T: serde::Serialize,
-    {
-        todo!()
-    }
-
-    fn end(self) -> Result<Self::Ok, Self::Error> {
-        todo!()
+impl MapSerializer {
+    fn new() -> Self {
+        Self {
+            elements: Vec::new(),
+            key: None,
+        }
     }
 }
-
-pub struct TupleVariantSerializer;
-
-impl serde::ser::SerializeTupleVariant for TupleVariantSerializer {
-    type Ok = Value;
-    type Error = Error;
-
-    fn serialize_field<T: ?Sized>(&mut self, value: &T) -> Result<(), Self::Error>
-    where
-        T: serde::Serialize,
-    {
-        todo!()
-    }
-
-    fn end(self) -> Result<Self::Ok, Self::Error> {
-        todo!()
-    }
-}
-
-pub struct MapSerializer;
 
 impl serde::ser::SerializeMap for MapSerializer {
     type Ok = Value;
@@ -253,24 +234,89 @@ impl serde::ser::SerializeMap for MapSerializer {
     where
         T: serde::Serialize,
     {
-        todo!()
+        let key = to_value(key)?;
+        self.key = Some(key);
+        Ok(())
     }
 
     fn serialize_value<T: ?Sized>(&mut self, value: &T) -> Result<(), Self::Error>
     where
         T: serde::Serialize,
     {
-        todo!()
+        let key = self.key.take().ok_or(Error::MissingMapKey)?;
+        let value = to_value(value)?;
+        self.elements.push((key, value));
+        Ok(())
     }
 
     fn end(self) -> Result<Self::Ok, Self::Error> {
-        todo!()
+        Ok(Value::Map(self.elements))
     }
 }
 
-pub struct StructSerializer;
+pub struct TupleSerializer {
+    name: Option<String>,
+    members: Vec<TupleMember>,
+}
 
-impl serde::ser::SerializeStruct for StructSerializer {
+impl TupleSerializer {
+    fn new(name: Option<String>) -> Self {
+        Self {
+            name,
+            members: Vec::new(),
+        }
+    }
+
+    fn add_member<T: ?Sized>(&mut self, name: Option<String>, value: &T) -> Result<(), Error>
+    where
+        T: serde::Serialize,
+    {
+        let value = to_value(value)?;
+        self.members.push(TupleMember { name, value });
+        Ok(())
+    }
+
+    fn into_value(self) -> Value {
+        Value::Tuple {
+            name: self.name,
+            members: self.members,
+        }
+    }
+}
+
+impl serde::ser::SerializeTuple for TupleSerializer {
+    type Ok = Value;
+    type Error = Error;
+
+    fn serialize_element<T: ?Sized>(&mut self, value: &T) -> Result<(), Self::Error>
+    where
+        T: serde::Serialize,
+    {
+        self.add_member(None, value)
+    }
+
+    fn end(self) -> Result<Self::Ok, Self::Error> {
+        Ok(self.into_value())
+    }
+}
+
+impl serde::ser::SerializeTupleStruct for TupleSerializer {
+    type Ok = Value;
+    type Error = Error;
+
+    fn serialize_field<T: ?Sized>(&mut self, value: &T) -> Result<(), Self::Error>
+    where
+        T: serde::Serialize,
+    {
+        self.add_member(None, value)
+    }
+
+    fn end(self) -> Result<Self::Ok, Self::Error> {
+        Ok(self.into_value())
+    }
+}
+
+impl serde::ser::SerializeStruct for TupleSerializer {
     type Ok = Value;
     type Error = Error;
 
@@ -282,32 +328,72 @@ impl serde::ser::SerializeStruct for StructSerializer {
     where
         T: serde::Serialize,
     {
-        todo!()
+        self.add_member(Some(key.to_string()), value)
     }
 
     fn end(self) -> Result<Self::Ok, Self::Error> {
-        todo!()
+        Ok(self.into_value())
     }
 }
 
-pub struct StructVariantSerializer;
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::collections::BTreeMap;
 
-impl serde::ser::SerializeStructVariant for StructVariantSerializer {
-    type Ok = Value;
-    type Error = Error;
-
-    fn serialize_field<T: ?Sized>(
-        &mut self,
-        key: &'static str,
-        value: &T,
-    ) -> Result<(), Self::Error>
-    where
-        T: serde::Serialize,
-    {
-        todo!()
+    #[test]
+    fn map_to_value() {
+        let mut map = BTreeMap::new();
+        map.insert(1, "hello".to_string());
+        map.insert(2, "world".to_string());
+        let value = to_value(&map).expect("serialization error");
+        let expected = Value::Map(vec![
+            (Value::Int32(1), Value::String("hello".to_string())),
+            (Value::Int32(2), Value::String("world".to_string())),
+        ]);
+        assert_eq!(value, expected);
     }
 
-    fn end(self) -> Result<Self::Ok, Self::Error> {
-        todo!()
+    #[test]
+    fn struct_to_value() {
+        #[derive(serde::Serialize)]
+        struct S {
+            a: i32,
+            b: f32,
+            c: bool,
+            d: Vec<&'static str>,
+        };
+        let s = S {
+            a: 320,
+            b: 1293.32,
+            c: false,
+            d: vec!["cookies", "muffins"],
+        };
+        let value = to_value(&s).expect("serialization error");
+        let expected = Value::Tuple {
+            name: Some("S".to_string()),
+            members: vec![
+                TupleMember {
+                    name: Some("a".to_string()),
+                    value: Value::Int32(320),
+                },
+                TupleMember {
+                    name: Some("b".to_string()),
+                    value: Value::Float(1293.32),
+                },
+                TupleMember {
+                    name: Some("c".to_string()),
+                    value: Value::Bool(false),
+                },
+                TupleMember {
+                    name: Some("d".to_string()),
+                    value: Value::List(vec![
+                        Value::String("cookies".to_string()),
+                        Value::String("muffins".to_string()),
+                    ]),
+                },
+            ],
+        };
+        assert_eq!(value, expected);
     }
 }
